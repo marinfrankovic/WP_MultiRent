15c3c9e438cfacf861963b3689f34fce71cff18e
mrafsi <mrafsi@github.com>
2026-05-17 09:27:44 +0200

    Remove unused local WordPress test helpers

--- local-wordpress/apply-menu-test.php
+++
@@ -1,3 +0,0 @@
-<?php
-multirent_companion_apply_top_menu("Home | /\nRentals | /rentals/\nAmenities | /amenity/\nContact | #contact");
-echo "menu-applied\n";
--- local-wordpress/install-recommended-test.php
+++
@@ -1,3 +0,0 @@
-<?php
-echo multirent_companion_install_recommended_plugins();
-echo "\n";
